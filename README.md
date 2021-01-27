2fbc82709ad0b7b97a632c29169ce11c904b605b
sudakogoma <sudako.goma@gmail.com>
2021-01-27 23:39:16 +0900

    プロジェクト ファイルを追加します。

---
+++ README.md
@@ -0,0 +1,9 @@
+# TrialWebView2
+
+## Overview
+
+Try WebView2 with WinForm.
+
+## Licence
+
+[MIT](https://github.com/sudakogoma/TrialWebView2/blob/master/LICENSE)
---
+++ src/TrialWebView2/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TrialWebView2
+{
+    /// <summary>
+    /// WewView2 お試し
+    /// 
+    /// WebView2 Runtime が必要
+    /// https://docs.microsoft.com/ja-jp/microsoft-edge/webview2/concepts/distribution
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.webView.Source = new Uri("http://acid3.acidtests.org/");
+        }
+
+        private void webView_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            this.webView.Visible = false;
+            this.progressBar1.Visible = true;
+        }
+
+        private void webView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            this.progressBar1.Visible = false;
+            this.webView.Visible = true;
+        }
+    }
+}
